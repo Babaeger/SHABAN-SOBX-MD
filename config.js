e52eade3a223d45c2878e48e99e1617c93347602
Babaeger <hubgit672@gmail.com>
2024-12-27 15:39:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "TNhW3LQb#FPuwJwm-IPJlAoJ6M_7JCmRAXicgG7EYMryZsWE5XiY",
     CAPTION: process.env.CAPTION || "POWERED BY MRSHABAN432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
