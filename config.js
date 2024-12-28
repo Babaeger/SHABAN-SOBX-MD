80d014856464befb6adb8f86d64fda2b1726f02d
Babaeger <hubgit672@gmail.com>
2024-12-28 14:29:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ module.exports = {
     CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
     AUTO_REACT: process.env.AUTO_REACT || "false",
     HEART_REACT: process.env.HEART_REACT || "false",
-    OWNER_REACT: process.env.OWNER_REACT || "true",
+    OWNER_REACT: process.env.OWNER_REACT || "false",
     BOT_NAME: process.env.BOT_NAME || "ᴍʀ_sʜᴀʙᴀɴ",
     STATUS_REPLY: process.env.STATUS_REPLY || "`Hacked`",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
